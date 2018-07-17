edb38de4cc15bc257d7baa077fa01a35f55e9519
Luke Horgan <horgan.l@husky.neu.edu>
2018-07-17 15:24:45 -0400

    small things

--- src/kernel/network.cpp
+++ src/kernel/network.cpp
@@ -171,13 +171,13 @@ CryptoKernel::Network::Network(CryptoKernel::Log* log,
     connectionThread.reset(new std::thread(&CryptoKernel::Network::connectionFunc, this));
 
     // Start management thread
-    networkThread.reset(new std::thread(&CryptoKernel::Network::networkFunc, this));
+    //networkThread.reset(new std::thread(&CryptoKernel::Network::networkFunc, this));
 
     // Start peer thread
-   	makeOutgoingConnectionsThread.reset(new std::thread(&CryptoKernel::Network::makeOutgoingConnectionsWrapper, this));
+   	//makeOutgoingConnectionsThread.reset(new std::thread(&CryptoKernel::Network::makeOutgoingConnectionsWrapper, this));
 
     // Start peer thread
-    infoOutgoingConnectionsThread.reset(new std::thread(&CryptoKernel::Network::infoOutgoingConnectionsWrapper, this));
+    //infoOutgoingConnectionsThread.reset(new std::thread(&CryptoKernel::Network::infoOutgoingConnectionsWrapper, this));
 }
 
 CryptoKernel::Network::~Network() {
