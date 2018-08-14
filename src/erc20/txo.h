ce972aaf9eaa6a4ee9fafd7b4948b66e983fb542
Luke Horgan <horgan.l@husky.neu.edu>
2018-08-14 11:51:52 -0400

    txos

---
+++ src/erc20/txo.h
@@ -0,0 +1,21 @@
+#include <string>
+
+#include "network.h"
+
+class Txo {
+public:
+    Txo(const std::string& id, const uint64_t value);
+    Txo(const Json::Value& txoJson);
+
+    Json::Value toJson() const;
+
+    bool isSpent() const;
+    void spend();
+
+    uint64_t getValue() const;
+
+private:
+    std::string id;
+    bool spent;
+    uint64_t value;
+};
--- src/erc20/wallet.cpp
+++ src/erc20/wallet.cpp
@@ -1,3 +1,6 @@
+#include <random>
+
+#include "crypto.h"
 #include "wallet.h"
 
 Wallet::Wallet() {
@@ -12,5 +15,18 @@ Wallet::Wallet() {
 }
 
 bool Wallet::transfer(const std::string& pubKey, uint64_t value) {
+    // set up the uniform random distribution
+    const time_t t = std::time(0);
+    const uint64_t now = static_cast<uint64_t> (t);;
+    default_random_engine generator(now);
+    uniform_int_distribution<unsigned int> distribution(0, UINT_MAX);
+    const uint64_t nonce = distribution(generator);
 
+    Json::Value data;
+    data["publicKey"] = pubKey;
+
+    const CryptoKernel::Blockchain::output toThem = CryptoKernel::Blockchain::output(50, distribution(generator), data);
+    vector<CryptoKernel::Blockchain::transaction> transactions;
+    //transactions.push_back(output);
+    network->broadcastTransactions(transactions);
 }
