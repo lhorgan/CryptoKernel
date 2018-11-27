2bfdef93558796e180b4f3f36335b8eadf4410e3
Luke Horgan <horgan.l@husky.neu.edu>
2018-11-27 13:52:18 -0500

    keep at it

--- src/kernel/consensus/RaftNet.h
+++ src/kernel/consensus/RaftNet.h
@@ -141,7 +141,7 @@ private:
             }
             //printf("Running...\n");
             // Make the selector wait for data on any socket
-            if(selector.wait(sf::milliseconds(50))) {
+            //if(selector.wait(sf::milliseconds(50))) {
                 // Test the listener
                 if(selector.isReady(listener)) {
                     //printf("we're ready here\n");
@@ -205,10 +205,10 @@ private:
                         }
                     }
                 }
-            }
+            /*}
             else {
                 printf("Something is wrong with the selector\n");
-            }
+            }*/
         }
     }
 };
